e9c9fb0c08ce371b5a53771d8b220f46631a87fd
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-03-15 23:31:31 -0400

    Godot Screen

--- Scripts/godotSplashscreen.gd
+++ Scripts/godotSplashscreen.gd
@@ -8,9 +8,12 @@ extends Control
 var skipTimer : float = 0;
 var skipVisible : bool = false
 
+var startScene : String = "res://Scenes/Main.tscn"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if OS.is_debug_build():
+		get_tree().change_scene_to_file(startScene)
 
 func _process(delta):
 	if (skipTimer > 0):
@@ -21,7 +24,7 @@ func _process(delta):
 			skipVisible = false
 
 func _on_animation_player_animation_finished(_anim_name):
-	get_tree().change_scene_to_file("res://Scenes/Screens/TitleScreen.tscn")
+	get_tree().change_scene_to_file(startScene)
 
 func _input(event : InputEvent):
 	if (event is InputEventKey and event.keycode == KEY_ESCAPE) or (event is InputEventMouseButton and event.is_double_click()):
@@ -29,7 +32,7 @@ func _input(event : InputEvent):
 			transitionToBlack.play("FadeOut")
 			buttonSound.play()
 			await transitionToBlack.animation_finished
-			get_tree().change_scene_to_file("res://Scenes/Screens/TitleScreen.tscn")
+			get_tree().change_scene_to_file(startScene)
 	elif (event is InputEventMouseMotion):
 		#play animation only if not already visible
 		if (!skipVisible):
