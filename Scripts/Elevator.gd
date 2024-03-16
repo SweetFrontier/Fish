52df090cd509dc15c9d3609362090364a3746671
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-03-15 21:46:34 -0400

    Elevator Function

--- Scripts/Elevator.gd
+++ Scripts/Elevator.gd
@@ -4,17 +4,25 @@ extends Area2D
 var fish_here : bool = false
 var moving_down : bool = false	
 
+
 func _physics_process(delta):
 	if (moving_down):
 		$"../AnimationPlayer".play("level_down")
+		moving_down = false
+
 
 func _input(event):
 	if (fish_here and Input.is_key_pressed(KEY_F)):
-		$"../Blocker/CollisionPolygon2D".disabled = false
+		$TextureRect.texture = load("res://Art/ElevatorClosed.png")
+		$TextureRect.z_index = 11
+		$"../Blocker/FloorCollision".disabled = true
+		$"../Blocker/BucketCollision".disabled = false
+		moving_down = true
 
-func _on_body_entered(body):
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	fish_here = true
 
 
-func _on_body_exited(body):
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	fish_here = false
