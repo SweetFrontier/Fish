5fd58aef6e0c512aca7e5b515a65cea688ad406e
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-03-16 20:44:53 -0400

    Elevator

--- Scripts/Elevator.gd
+++ Scripts/Elevator.gd
@@ -13,11 +13,12 @@ func _physics_process(delta):
 
 func _input(event):
 	if (fish_here and Input.is_key_pressed(KEY_F)):
-		$TextureRect.texture = load("res://Art/ElevatorClosed.png")
+		$TextureRect.texture = load("res://Art/Elevator/ElevatorClosed.png")
 		$TextureRect.z_index = 11
 		$"../Blocker/FloorCollision".disabled = true
 		$"../Blocker/BucketCollision".disabled = false
 		moving_down = true
+		$TextureRect2.visible = true
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
@@ -26,3 +27,8 @@ func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, 
 
 func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	fish_here = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if (anim_name == "level_down"):
+		$Node.visible = true
